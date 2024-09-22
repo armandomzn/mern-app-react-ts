a797daa5e659e3b1d1852a5d2e204e748cbcd49c
Armando <armandomm98@hotmail.com>
2024-09-22 01:19:52 -0600

    The toast error message is refactored.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,11 +1,11 @@
 import { AxiosResponse, isAxiosError } from "axios";
 import { LoaderFunction, redirect, useLoaderData } from "react-router-dom";
 import { agent } from "../api/agent";
-import { toast } from "react-toastify";
 import { AppStatsProps } from "../interfaces";
 import { Wrapper } from "../assets/wrappers/StatContainer";
 import StatItem from "../components/StatItem";
 import { FaSuitcase, FaUsers } from "react-icons/fa";
+import { showToast } from "../utils/showToast";
 
 export const loaderAdmin: LoaderFunction = async () => {
   try {
@@ -13,12 +13,8 @@ export const loaderAdmin: LoaderFunction = async () => {
     return data;
   } catch (error) {
     if (isAxiosError(error)) {
-      const errorMessage = Array.isArray(error?.response?.data?.message)
-        ? error?.response?.data.message
-            .map((message: string) => message)
-            .join(",")
-        : error?.response?.data.message;
-      toast.error(errorMessage, { autoClose: 5000 });
+      const errorMessage = error?.response?.data?.message;
+      showToast("admin-loader-error", errorMessage, "error");
     }
     return redirect("/dashboard");
   }
