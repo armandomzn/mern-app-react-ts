299c3ce836cbcbfb8cb7e577497f9d766774393a
Armando <armandomm98@hotmail.com>
2024-09-22 17:49:24 -0600

    Component refactored for correct handling of pages not found at home or on the dashboard determined whether a user exists or not.

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,8 +1,27 @@
-import { isRouteErrorResponse, useRouteError, Link } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useRouteError,
+  Link,
+} from "react-router-dom";
 import { Wrapper } from "../assets/wrappers/ErrorPage";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import { UserPayload } from "../interfaces";
+import { agent } from "../api/agent";
+
+const userQuery = {
+  queryKey: ["current-user"],
+  queryFn: async () => {
+    const { data }: AxiosResponse<UserPayload> =
+      await agent.User.getCurrentUser();
+    return data;
+  },
+};
 
 const Error = () => {
   const error = useRouteError();
+  const { data:user } = useQuery(userQuery);
+
   if (isRouteErrorResponse(error)) {
     if (error.status === 404) {
       return (
@@ -11,7 +30,9 @@ const Error = () => {
             <h1>404</h1>
             <h4>Opps! Page not found</h4>
             <p>Sorry, the page you tried cannot be found</p>
-            <Link to="/">back home</Link>
+            <Link to={user ? "/dashboard" : "/"}>
+              {user ? "back to dashboard" : "back home"}
+            </Link>
           </div>
         </Wrapper>
       );
