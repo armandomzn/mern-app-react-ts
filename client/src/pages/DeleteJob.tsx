1d8f4c07b0a1893044a9ddcc8f0a351c085cf563
Armando <armandomm98@hotmail.com>
2024-09-22 23:10:28 -0600

    Canceling jobs and the current job when one is deleted to refresh cache data and enhanced toast messages.

--- client/src/pages/DeleteJob.tsx
+++ client/src/pages/DeleteJob.tsx
@@ -1,22 +1,23 @@
 import { AxiosResponse, isAxiosError } from "axios";
 import { ActionFunction, redirect } from "react-router-dom";
 import { agent } from "../api/agent";
-import { toast } from "react-toastify";
+import { QueryClient } from "@tanstack/react-query";
+import { showToast } from "../utils/showToast";
 
-export const deleteJobAction: ActionFunction = async ({ params }) => {
-  try {
-    const { id } = params;
-    const { data }: AxiosResponse = await agent.Jobs.deleteJob(id as string);
-    toast.success(data?.message);
-  } catch (error) {
-    if (isAxiosError(error)) {
-      const errorMessage = Array.isArray(error?.response?.data?.message)
-        ? error?.response?.data.message
-            .map((message: string) => message)
-            .join(",")
-        : error?.response?.data.message;
-      toast.error(errorMessage, { autoClose: 5000 });
+export const deleteJobAction =
+  (queryClient: QueryClient): ActionFunction =>
+  async ({ params }) => {
+    try {
+      const { id } = params;
+      const { data }: AxiosResponse = await agent.Jobs.deleteJob(id as string);
+      await queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      await queryClient.invalidateQueries({ queryKey: ["job", id] });
+      showToast("delete-job", data?.message);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const errorMessage: string | string[] = error?.response?.data?.message;
+        showToast("delete-job-error", errorMessage, "error");
+      }
     }
-  }
-  return redirect("/dashboard/all-jobs");
-};
+    return redirect("/dashboard/all-jobs");
+  };
