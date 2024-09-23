a3564392a43235f8bd841cedae3cfed2efba4733
Armando <armandomm98@hotmail.com>
2024-09-22 22:28:51 -0600

    Added a reference to clean the file type input, and the way the form is submitted using the hook useSubmit, optimized the SubmitBtn to correctly handle the form status using formAction, optimized the toast messages for actions and invalidate the user in the queries to return fresh data.

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -4,51 +4,65 @@ import {
   Link,
   redirect,
   useOutletContext,
+  useSubmit,
 } from "react-router-dom";
 import { Wrapper } from "../assets/wrappers/DashboardFormPage";
 import { ContextType } from "./DashboardLayout";
 import { FormRow, SubmitBtn } from "../components";
-import { toast } from "react-toastify";
 import { AxiosResponse, isAxiosError } from "axios";
 import { agent } from "../api/agent";
+import { QueryClient } from "@tanstack/react-query";
+import { showToast } from "../utils/showToast";
+import { useRef } from "react";
 
-export const profileAction: ActionFunction = async ({ request }) => {
-  try {
-    const formData = await request.formData();
-    const { data }: AxiosResponse = await agent.User.updateUser(formData);
-    toast.success(data?.message);
-    return redirect("/dashboard/profile");
-  } catch (error) {
-    if (isAxiosError(error)) {
-      const errorMessage = Array.isArray(error?.response?.data?.message)
-        ? error?.response?.data.message
-            .map((message: string) => message)
-            .join(",")
-        : error?.response?.data.message;
-      toast.error(errorMessage, { autoClose: 5000 });
+export const profileAction =
+  (queryClient: QueryClient): ActionFunction =>
+  async ({ request }) => {
+    try {
+      const formData = await request.formData();
+      const { data }: AxiosResponse = await agent.User.updateUser(formData);
+      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
+      showToast("profile", data?.message);
+      return redirect("/dashboard/profile");
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const errorMessage: string | string[] = error?.response?.data?.message;
+        showToast("profile-error", errorMessage, "error");
+      }
+      return error;
     }
-    return error;
-  }
-};
+  };
 
-export const deleteProfileAction: ActionFunction = async () => {
-  try {
-    const { data }: AxiosResponse = await agent.User.deleteProfileImage();
-    toast.success(data?.message);
-    return redirect("/dashboard/profile");
-  } catch (error) {
-    if (isAxiosError(error)) {
-      const errorMessage = Array.isArray(error?.response?.data?.message)
-        ? error?.response?.data.message[0]
-        : error?.response?.data.message;
-      toast.error(errorMessage, { autoClose: 5000 });
+export const deleteProfileAction =
+  (queryClient: QueryClient): ActionFunction =>
+  async () => {
+    try {
+      const { data }: AxiosResponse = await agent.User.deleteProfileImage();
+      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
+      showToast("profile-image", data?.message);
+      return redirect("/dashboard/profile");
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const errorMessage: string | string[] = error?.response?.data?.message;
+        showToast("profile-image-error", errorMessage, "error");
+      }
+      return error;
     }
-    return error;
-  }
-};
+  };
 
 const Profile = () => {
   const { user, isDarkTheme } = useOutletContext() as ContextType;
+  const submit = useSubmit();
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    submit(e.currentTarget, { method: "PATCH" });
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
   return (
     <Wrapper $isDarkTheme={isDarkTheme}>
       <h3>profile</h3>
@@ -68,18 +82,29 @@ const Profile = () => {
                 nameState={`remove-image-profile-submit`}
                 optionalButtonText="delete image"
                 optionalClassName="btn-full"
+                currentFormAction="/dashboard/delete-profile-image"
               />
             </div>
           </div>
         </Form>
       )}
-      <Form method="PATCH" encType="multipart/form-data">
+      <Form
+        method="PATCH"
+        encType="multipart/form-data"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <div className="form-row">
           <label htmlFor="avatar" className="form-label">
             {user.avatar ? "replace" : "select"} an image file (max 0.5MB |
             optional)
           </label>
-          <input type="file" name="avatar" id="avatar" accept="image/*" />
+          <input
+            type="file"
+            name="avatar"
+            id="avatar"
+            accept="image/*"
+            ref={fileInputRef}
+          />
         </div>
         <FormRow name="name" defaultValue={user.name} type="text" />
         <FormRow
@@ -88,7 +113,7 @@ const Profile = () => {
           type="text"
           textLabel="last name"
         />
-        {/* <FormRow name="email" defaultValue={user.email} type="email" /> */}
+        <FormRow name="email" defaultValue={user.email} type="email" />
         <FormRow name="location" defaultValue={user.location} type="location" />
         <Link
           to="update-password"
@@ -101,6 +126,7 @@ const Profile = () => {
           nameState={`update-profile-submit`}
           optionalButtonText="update profile"
           optionalClassName="form-btn"
+          currentFormAction="/dashboard/profile"
         />
       </Form>
     </Wrapper>
