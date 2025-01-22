4851ef15d986a38be3ccba3d54f233da6a3a3fa0
Armando <armandomm98@hotmail.com>
2025-01-22 13:43:13 -0600

    The format in which the CustomRequest type is displayed has been changed.

--- backend/src/interfaces/CustomRequestType.ts
+++ backend/src/interfaces/CustomRequestType.ts
@@ -1,4 +1,6 @@
 import { Request } from "express";
 import { JwtPayload } from "./JwtPayloadProps";
 
-export type CustomRequest = Request & { user: JwtPayload };
+export type CustomRequest = Request & {
+  user: JwtPayload;
+};
