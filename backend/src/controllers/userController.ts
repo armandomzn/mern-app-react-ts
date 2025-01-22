c6a905e8d240bdf2f577d97ca11a135685b6762a
Armando <armandomm98@hotmail.com>
2025-01-22 17:54:39 -0600

    In the user controller the method to update the user is refactored, a transaction is used to update the user and refresh the access and refresh token because the user information changes if the profile is updated, the refresh token is also updated in the schema.

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,15 +1,20 @@
-import { Response } from "express";
 import fs from "fs";
+import mongoose from "mongoose";
+import cloudinary from "cloudinary";
+import { Response } from "express";
 import { CustomRequest, JwtPayload } from "../interfaces";
 import { JobSchema, TokenSchema, UserSchema } from "../models";
 import { StatusCodes } from "http-status-codes";
-import cloudinary from "cloudinary";
-import crypto from "crypto";
 import {
-  ACCESS_TOKEN_EXPIRY,
   REFRESH_TOKEN_EXPIRY_MS,
 } from "../helpers/constants";
-import { setAuthCookies, createJWT, hashPassword } from "../helpers";
+import {
+  setAuthCookies,
+  hashPassword,
+  createRefreshJWT,
+  createAccessJWT,
+} from "../helpers";
+import { BadRequestError } from "../errors/customErrors";
 
 const getCurrentUser = async (req: CustomRequest, res: Response) => {
   const user = await UserSchema.findOne({ _id: req.user.userId });
@@ -26,61 +31,68 @@ const getApplicationStats = async (req: CustomRequest, res: Response) => {
 };
 
 const updateUser = async (req: CustomRequest, res: Response) => {
-  if (req.file) {
-    // {
-    //   fieldname: 'avatar',
-    //   originalname: 'demo.jpg',
-    //   encoding: '7bit',
-    //   mimetype: 'image/jpeg',
-    //   destination: 'public/uploads',
-    //   filename: 'buttons.jpg',
-    //   path: 'public\\uploads\\buttons.jpg',
-    //   size: 86121
-    // }
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    // we remove the uploaded image from public/uploads after we upload it in cloudinary
-    await fs.promises.unlink(req.file.path);
-    req.body.avatar = response.secure_url;
-    req.body.avatarPublicId = response.public_id;
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    if (req.file) {
+      // {
+      //   fieldname: 'avatar',
+      //   originalname: 'demo.jpg',
+      //   encoding: '7bit',
+      //   mimetype: 'image/jpeg',
+      //   destination: 'public/uploads',
+      //   filename: 'buttons.jpg',
+      //   path: 'public\\uploads\\buttons.jpg',
+      //   size: 86121
+      // }
+      const response = await cloudinary.v2.uploader.upload(req.file.path);
+      // we remove the uploaded image from public/uploads after we upload it in cloudinary
+      await fs.promises.unlink(req.file.path);
+      req.body.avatar = response.secure_url;
+      req.body.avatarPublicId = response.public_id;
+    }
+
+    const user = await UserSchema.findByIdAndUpdate(req.user.userId, req.body);
+
+    // If image exist in the current user that means that we are replacing the current image, we are not using the {new:true} options in findByIdAndUpdate method because we want the last reference to the user a not the new one to have access to the avatarPublicId property to properly delete the image from cloudinary
+    if (req.file && user.avatarPublicId) {
+      await cloudinary.v2.uploader.destroy(user.avatarPublicId);
+    }
+
+    // We recreate JWT
+    const userPayload: JwtPayload = {
+      userId: user._id,
+      userName: user.userName,
+      role: user.role,
+    };
+
+    const refreshTokenJWT = createRefreshJWT({
+      userId: req.user.userId,
+    });
+
+    await TokenSchema.findOneAndUpdate(
+      {
+        user: user._id,
+      },
+      {
+        refreshToken: refreshTokenJWT,
+        expiresAt: new Date(Date.now() + REFRESH_TOKEN_EXPIRY_MS),
+      }
+    );
+
+    const accessTokenJWT = createAccessJWT(userPayload);
+
+    setAuthCookies(res, accessTokenJWT, refreshTokenJWT);
+    await session.commitTransaction();
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "User Profile Updated Successfully" });
+  } catch (error) {
+    await session.abortTransaction();
+    throw new BadRequestError("Failed transaction when updating user");
+  } finally {
+    await session.endSession();
   }
-
-  const user = await UserSchema.findByIdAndUpdate(req.user.userId, req.body);
-
-  // If image exist in the current user that means that we are replacing the current image, we are not using the {new:true} options in findByIdAndUpdate method because we want the last reference to the user a not the new one to have access to the avatarPublicId property to properly delete the image from cloudinary
-  if (req.file && user.avatarPublicId) {
-    await cloudinary.v2.uploader.destroy(user.avatarPublicId);
-  }
-
-  // We recreate JWT
-  const userPayload: JwtPayload = {
-    userId: user._id,
-    userName: user.userName,
-    role: user.role,
-  };
-
-  const newRefreshToken = crypto.randomBytes(40).toString("hex");
-  await TokenSchema.findOneAndUpdate(
-    {
-      user: user._id,
-    },
-    {
-      refreshToken: newRefreshToken,
-      expiresAt: new Date(Date.now() + REFRESH_TOKEN_EXPIRY_MS),
-    },
-    { new: true, upsert: true }
-  );
-
-  const accessTokenJWT = createJWT(userPayload, ACCESS_TOKEN_EXPIRY);
-  const refreshTokenJWT = createJWT({
-    ...userPayload,
-    refreshToken: newRefreshToken,
-  });
-
-  setAuthCookies(res, accessTokenJWT, refreshTokenJWT);
-
-  return res
-    .status(StatusCodes.OK)
-    .json({ message: "User Profile Updated Successfully" });
 };
 
 const deleteProfileImage = async (req: CustomRequest, res: Response) => {
