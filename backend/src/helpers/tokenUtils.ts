b37a2a9f43e80b70f131f821292303e7b193f864
Armando <armandomm98@hotmail.com>
2025-01-22 17:17:39 -0600

    The methods for token creation were refactored, separating one for the creation of the access token and another for the refresh, also the method for the verification of the token was modified to check if it is an access token or a refresh token to use its respective password.

--- backend/src/helpers/tokenUtils.ts
+++ backend/src/helpers/tokenUtils.ts
@@ -1,16 +1,33 @@
 import jwt from "jsonwebtoken";
 import { JwtPayload } from "../interfaces";
+import { BadRequestError } from "../errors/customErrors";
 
-const createJWT = (payload: JwtPayload, expiresIn?: string | undefined) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: expiresIn || process.env.JWT_EXPIRES_IN,
+const createAccessJWT = (payload: JwtPayload) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN || "6m",
   });
   return token;
 };
 
-const verifyJWT = (token: string) => {
-  const isValidToken = jwt.verify(token, process.env.JWT_SECRET);
-  return isValidToken;
+const createRefreshJWT = (payload: JwtPayload) => {
+  const token = jwt.sign(
+    payload,
+    process.env.JWT_SECRET_REFRESH_TOKEN as string,
+    {
+      expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN || "1d",
+    }
+  );
+  return token;
 };
 
-export { createJWT, verifyJWT };
+const verifyJWT = (token: string, isRefreshToken = false) => {
+  const secretKey = isRefreshToken
+    ? process.env.JWT_SECRET_REFRESH_TOKEN
+    : process.env.JWT_SECRET;
+  if (!secretKey) {
+    throw new BadRequestError("Missing secret key for token verification.");
+  }
+  return jwt.verify(token, secretKey as string) as JwtPayload;
+};
+
+export { createAccessJWT, createRefreshJWT, verifyJWT };
