c9ffd411b7836bcf649784e3a0dabddde704c169
Armando <armandomm98@hotmail.com>
2025-01-22 17:57:14 -0600

    New variables have been added to the main index.ts file.

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -1,4 +1,15 @@
-import { setAuthCookies, setCookie } from "./cookieUtils";
+import {
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_EXPIRY,
+  COOKIE_EXPIRES_ONE_DAY_MS,
+  JOB_SORT,
+  JOB_STATUS,
+  JOB_TYPE,
+  PASSWORD_TEN_MINUTES_EXPIRY,
+  REFRESH_TOKEN,
+  REFRESH_TOKEN_EXPIRY_MS,
+} from "./constants";
+import { clearCookies, setAuthCookies, setCookie } from "./cookieUtils";
 import { createHashToken } from "./createHashToken";
 import { Pagination } from "./Pagination";
 import { comparePassword, hashPassword } from "./passwordUtils";
@@ -6,11 +17,13 @@ import { sendEmail } from "./sendEmail";
 import { sendResetPasswordEmail } from "./sendResetPasswordEmail";
 import { sendResetSuccessPasswordEmail } from "./sendResetSuccessPasswordEmail";
 import { sendVerificationEmail } from "./sendVerificationEmail";
-import { createJWT, verifyJWT } from "./tokenUtils";
+import { createAccessJWT, createRefreshJWT, verifyJWT } from "./tokenUtils";
+import { VALIDATION_MESSAGES } from "./validationMessages";
 
 export {
   setAuthCookies,
   setCookie,
+  clearCookies,
   createHashToken,
   Pagination,
   hashPassword,
@@ -20,5 +33,16 @@ export {
   sendResetSuccessPasswordEmail,
   sendVerificationEmail,
   verifyJWT,
-  createJWT,
+  createAccessJWT,
+  createRefreshJWT,
+  ACCESS_TOKEN,
+  REFRESH_TOKEN,
+  ACCESS_TOKEN_EXPIRY,
+  COOKIE_EXPIRES_ONE_DAY_MS,
+  PASSWORD_TEN_MINUTES_EXPIRY,
+  REFRESH_TOKEN_EXPIRY_MS,
+  VALIDATION_MESSAGES,
+  JOB_STATUS,
+  JOB_TYPE,
+  JOB_SORT,
 };
