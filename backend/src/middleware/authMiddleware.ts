ce1c7770aac37c713d734c44ab50e72308234074
Armando <armandomm98@hotmail.com>
2025-01-22 18:39:03 -0600

    Fixed a bug in the user session by creating a function to rotate the tokens and save the refresh token correctly in the schema, for this a transaction is used and waits until the access and refresh tokens are created to be set in the cookies.

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,32 +1,34 @@
+import mongoose from "mongoose";
 import { Response, NextFunction } from "express";
 import {
   BadRequestError,
   UnauthenticatedError,
   UnauthorizedError,
 } from "../errors/customErrors";
-import crypto from "crypto";
-import { JwtPayload, CustomRequest } from "../interfaces";
-import { TokenSchema } from "../models";
-
+import { JwtPayload, CustomRequest, TokenDocument } from "../interfaces";
+import { TokenSchema, UserSchema } from "../models";
 import {
-  ACCESS_TOKEN_EXPIRY,
   REFRESH_TOKEN_EXPIRY_MS,
-} from "../helpers/constants";
-import { setAuthCookies, createJWT, verifyJWT } from "../helpers";
+  setAuthCookies,
+  createAccessJWT,
+  createRefreshJWT,
+  verifyJWT,
+  clearCookies,
+} from "../helpers";
 
 const authenticateUser = async (
   req: CustomRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const { accessToken, refreshToken } = req.signedCookies;
-  if (!accessToken && !refreshToken) {
+  const { ACCESS_TOKEN, REFRESH_TOKEN } = req.signedCookies;
+  if (!ACCESS_TOKEN && !REFRESH_TOKEN) {
     throw new UnauthenticatedError(`Authentication invalid`);
   }
   try {
     // If accessToken exist then that means we don't need the refreshToken for the moment and that cookie is still valid on the server
-    if (accessToken) {
-      const { userId, userName, role } = verifyJWT(accessToken) as JwtPayload;
+    if (ACCESS_TOKEN) {
+      const { userId, userName, role } = verifyJWT(ACCESS_TOKEN) as JwtPayload;
       req.user = {
         userId,
         role,
@@ -36,54 +38,59 @@ const authenticateUser = async (
       return next();
     }
     // If accessToken doesn't exist we need to rotate the refreshToken by creating a new one and delete the old one or obsolete one, this restarts the token cycle in the cookies and the session duration is extended
-    const payload = verifyJWT(refreshToken) as JwtPayload;
-    const existingToken = await TokenSchema.findOne({
+    const payload = verifyJWT(REFRESH_TOKEN, true) as JwtPayload;
+    const isRefreshingToken = await TokenSchema.findOne({
       user: payload.userId,
-      refreshToken: payload.refreshToken,
+      refreshToken: REFRESH_TOKEN,
     });
+    const user = await UserSchema.findById(payload.userId);
+
+    if (!isRefreshingToken) {
+      await TokenSchema.deleteOne({
+        user: payload.userId,
+        refreshToken: REFRESH_TOKEN,
+      });
+      clearCookies(res);
+      throw new UnauthorizedError("Invalid refresh token");
+    }
 
     // The isValid property is overridden by the database administrator in case the token or refreshToken has been compromised.
     if (
-      !existingToken ||
-      !existingToken.isValid ||
-      existingToken.expiresAt < new Date()
+      !isRefreshingToken.isValid ||
+      isRefreshingToken.expiresAt < new Date()
     ) {
+      await TokenSchema.deleteOne({
+        user: payload.userId,
+        refreshToken: REFRESH_TOKEN,
+      });
+      clearCookies(res);
       throw new UnauthenticatedError("Authentication Invalid");
     }
 
-    const newRefreshToken = crypto.randomBytes(40).toString("hex");
-    existingToken.refreshToken = newRefreshToken;
-    existingToken.expiresAt = new Date(Date.now() + REFRESH_TOKEN_EXPIRY_MS);
-    await existingToken.save();
-
-    // We create the access token with duration of 5 minutes
-    const accessTokenJWT = createJWT(
-      {
-        userId: payload.userId,
-        userName: payload.userName,
-        role: payload.role,
-      },
-      ACCESS_TOKEN_EXPIRY
+    const { accessTokenJWT, refreshTokenJWT } = await rotateRefreshToken(
+      isRefreshingToken,
+      payload
     );
 
-    // We create refreshToken with duration of oneDay by default, and we pass extra parameter which is the refreshToken
-    const refreshTokenJWT = createJWT({
-      userId: payload.userId,
-      userName: payload.userName,
-      role: payload.role,
-      refreshToken: newRefreshToken,
-    });
     // We create the cookie to store the accessToken and refreshToken
     setAuthCookies(res, accessTokenJWT, refreshTokenJWT);
 
     req.user = {
       userId: payload.userId,
-      role: payload.role,
-      userName: payload.userName,
-      testUser: payload.userId.toString() === "65d7d158fb173fc998d49247",
+      role: user.role,
+      userName: user.userName,
+      testUser: user._id.toString() === "65d7d158fb173fc998d49247",
     };
-    next();
+
+    return next();
   } catch (error) {
+    if (req.user.userId && REFRESH_TOKEN) {
+      await TokenSchema.deleteOne({
+        user: req.user.userId,
+        refreshToken: REFRESH_TOKEN,
+      });
+      clearCookies(res);
+    }
     throw new UnauthenticatedError("Authentication invalid");
   }
 };
@@ -106,6 +113,37 @@ const checkForTestUser = (
     throw new BadRequestError("Demo User, Read Only!");
   }
   next();
+};
+
+const rotateRefreshToken = async (
+  existingToken: TokenDocument,
+  payload: JwtPayload
+) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    // We save a new refreshToken for the user which is going to match with the current Token refreshToken
+    const refreshTokenJWT = createRefreshJWT({
+      userId: payload.userId,
+    });
+    existingToken.refreshToken = refreshTokenJWT;
+    existingToken.expiresAt = new Date(Date.now() + REFRESH_TOKEN_EXPIRY_MS);
+    await existingToken.save();
+
+    const user = await UserSchema.findById(payload.userId);
+
+    const accessTokenJWT = createAccessJWT({
+      userId: user._id,
+      userName: user.userName,
+      role: user.role,
+    });
+    await session.commitTransaction();
+    return { accessTokenJWT, refreshTokenJWT };
+  } catch (error) {
+    await session.abortTransaction();
+  } finally {
+    await session.endSession();
+  }
 };
 
 export { authenticateUser, authorizePermissions, checkForTestUser };
